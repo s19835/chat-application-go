5db0aafe639f218a480c1c8bfef6548c2ae669a3
s19835 <s19835@sci.pdn.ac.lk>
2024-10-20 20:02:58 +0530

    Add: comments

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"net/http"
 )
 
+// Listen to the path using net/http pkg
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Write out the HTML when a req is made
 		w.Write([]byte(
 			`<html>
 				<head>
@@ -19,7 +21,7 @@ func main() {
 		`))
 	})
 
-	// start the web-server
+	// Start the web-server on port 8080
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe", err)
 	}
